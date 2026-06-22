bf7fced001bb4ed363a21689a47ab73ae65c81a1
Daniel Bo <bodangren@gmail.com>
2026-06-23 05:15:47 +0800

    chore(sales): add sales_advantage DB to docker init (track_id: sales_advantage_mvp_20260622)

--- docker/init-db.sh
+++ docker/init-db.sh
@@ -7,7 +7,8 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE DATABASE primary_advantage;
     CREATE DATABASE science_advantage;
     CREATE DATABASE codecamp_advantage;
+    CREATE DATABASE sales_advantage;
     CREATE DATABASE science_advantage_test;
 EOSQL
 
-echo "✅ Created databases: reading_advantage, primary_advantage, science_advantage, codecamp_advantage, science_advantage_test"
+echo "✅ Created databases: reading_advantage, primary_advantage, science_advantage, codecamp_advantage, sales_advantage, science_advantage_test"
