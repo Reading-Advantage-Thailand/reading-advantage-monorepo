3632ad7664e897382b76d0a13c766bd57f2d1f94
Daniel Bo <bodangren@gmail.com>
2026-08-22 17:08:58 +0800

    chore(db): delete accounting database stream (track_id: accounting_product_simplification_20260822)

--- packages/db/src/accounting/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./environment.js";
-export * from "./schema/index.js";
-export * from "./client.js";
-export * from "./migration.js";
-export * from "./privileged.js";
--- packages/db/src/accounting/runtime.ts
+++
@@ -1,7 +0,0 @@
-/** Runtime-only accounting database client without migration tooling. */
-export { createAccountingRuntimeClient } from "./client.js";
-export {
-  accountingRuntimeEnvSchema,
-  createAccountingRuntimeConfig,
-  type AccountingRuntimeConfig,
-} from "./environment.js";
