b17145740d0b9e08a1cd5836e1d819c83bbb37ff
Daniel Bo <bodangren@gmail.com>
2026-09-11 22:17:00 +0800

    fix(reading): restore use client in matching components (track_id: broken_ux_fixes_20260911)

--- apps/reading-advantage/components/vocabulary/tab-matching-words.tsx
+++ apps/reading-advantage/components/vocabulary/tab-matching-words.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-// "use client";
+"use client";
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import dayjs from "dayjs";
